0acfae754e9b2bba8f95b4c808d23b837f3451d9
Voytov Stanislav <gonlliuk@gmail.com>
2017-08-27 20:01:01 +0500

    styles for product-card

--- src/js/apps/App/components/ProductCard/index.jsx
+++ src/js/apps/App/components/ProductCard/index.jsx
@@ -1,37 +1,34 @@
 import React, { Component } from 'react'
+import './index.styl'
 
 export default class extends Component {
-    getName() {
-        const {
-            product: {
-                name,
-                isNew,
-            }
-        } = this.props
-        return isNew ? `${name}, Новинка!` : name
-    }
     render() {
         const {
             product: {
                 color,
                 kind,
                 price,
+                isNew,
+                name,
             }
         } = this.props
         return <div className="product-card">
-            <div className="product-card__name">{::this.getName()}</div>
+            <div className="product-card__name">{name}</div>
             <div className="product-card__spec-list">
                 <div className="product-card__spec">
-                    <span className="product-card__spec-title">Вид</span>
+                    <span className="product-card__spec-title">Вид:</span>
                     <span className="product-card__spec-value">{kind}</span>
                 </div>
                 <div className="product-card__spec">
-                    <span className="product-card__spec-title">Цвет</span>
+                    <span className="product-card__spec-title">Цвет:</span>
                     <span className="product-card__spec-value">{color}</span>
                 </div>
+                <div className="product-card__spec">
+                    <span className="product-card__spec-title">Новинка:</span>
+                    <span className="product-card__spec-value">{isNew ? 'Да' : 'Нет'}</span>
+                </div>
             </div>
-            <div className="product-card__price">
-                <span className="product-card__spec-title">{price} P</span>
+            <div className="product-card__price">{price} P
             </div>
         </div>
     }

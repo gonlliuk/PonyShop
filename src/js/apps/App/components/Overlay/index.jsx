e2e56a0e4385b37126f69f1dee1526d050485e15
Voytov Stanislav <gonlliuk@gmail.com>
2017-08-27 21:01:41 +0500

    styles for overlay

--- src/js/apps/App/components/Overlay/index.jsx
+++ src/js/apps/App/components/Overlay/index.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 
 export default class extends Component {
     render() {
-        return <div>
-            <h3>Overlay</h3>
+        return <div className="overlay">
             { this.props.children}
         </div>
     }

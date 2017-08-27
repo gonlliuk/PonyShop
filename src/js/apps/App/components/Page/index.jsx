2084194a4cef2e5ecb25e60a141fb550347de5f1
Voytov Stanislav <gonlliuk@gmail.com>
2017-08-27 19:38:16 +0500

    add page component

--- src/js/apps/App/components/Page/index.jsx
+++ src/js/apps/App/components/Page/index.jsx
@@ -1,9 +1,72 @@
 import React, { Component } from 'react'
+import Paginator from '../Paginator'
+import ProductCard from '../ProductCard'
 
 export default class extends Component {
+    constructor(props) {
+        super(props)
+        this.pageLimit = 20
+        this.totalPages = Math.ceil(props.data.length / this.pageLimit)
+    }
+    getProductCardList() {
+        const {
+            page,
+            data,
+        } = this.props
+        const startPos = this.pageLimit * (page - 1)
+        const endPos = this.pageLimit * page
+        return data
+            .map((item, i) =>
+                <ProductCard
+                    key={item.name + i}
+                    product={item}/>
+            )
+            .slice(startPos, endPos)
+    }
+
+    nextPage() {
+        const {
+            history,
+            page,
+        } = this.props
+        history.push(`/${page + 1}`)
+    }
+
+    prevPage() {
+        const {
+            history,
+            page,
+        } = this.props
+        history.push(`/${page - 1}`)
+    }
+
+    setPage(page) {
+        const {
+            history,
+        } = this.props
+        history.push(`/${page}`)
+    }
+
     render() {
-        return <div>
-            <h3>Page</h3>
+        const {
+            toggleFilter,
+            page,
+        } = this.props
+        return <div className="page">
+            <div className="page__filter-buttons">
+                <button className="button" onClick={toggleFilter}>Настроить фильтры</button>
+            </div>
+            <div className="page__products">
+                { ::this.getProductCardList() }
+            </div>
+            <div className="page__paginator">
+                <Paginator
+                    setPage={::this.setPage}
+                    prevPage={::this.prevPage}
+                    nextPage={::this.nextPage}
+                    current={page}
+                    total={this.totalPages}/>
+            </div>
         </div>
     }
 }

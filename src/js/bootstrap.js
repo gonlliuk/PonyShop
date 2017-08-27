209c6c5980f43655d7b67fcce58d3d4d5f7e9ac8
Voytov Stanislav <gonlliuk@gmail.com>
2017-08-27 18:38:52 +0500

    add styles

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -2,6 +2,7 @@ import 'babel-polyfill'
 import 'react-hot-loader/patch'
 import generateData from './libs/generateData'
 import App from './apps/App'
+import '../stylus/main.styl'
 
 // Generate data for app
 const data = generateData()
